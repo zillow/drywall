423ba14f8a1d8f7b9bd68e8bb8d09833014e96bc
Brian Stone <brians@zillowgroup.com>
2019-06-22 13:22:50 -0700

    refactor: remove the boilerplate theme

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,36 +1,27 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Button, { StyledButton } from '../components/Button/Button';
-import Bootstrap from '../themes';
 
-describe('Themed Styled Button', () => {
+describe('Styled Button', () => {
     it('displays a button with requested text', () => {
-        const component = renderer.create(<StyledButton theme={Bootstrap}>Click Me!</StyledButton>);
+        const component = renderer.create(<StyledButton>Click Me!</StyledButton>);
         expect(component.toJSON()).toMatchSnapshot();
     });
 
     it('displays a secondary button with requested text', () => {
-        const component = renderer.create(
-            <StyledButton theme={Bootstrap} type="secondary">
-                ⊙_ʘ
-            </StyledButton>
-        );
+        const component = renderer.create(<StyledButton type="secondary">⊙_ʘ</StyledButton>);
         expect(component.toJSON()).toMatchSnapshot();
     });
 });
 
-describe('Themed Button', () => {
+describe('Button', () => {
     it('displays a button with requested text', () => {
-        const component = renderer.create(<Button theme={Bootstrap}>Click Me!</Button>);
+        const component = renderer.create(<Button>Click Me!</Button>);
         expect(component.toJSON()).toMatchSnapshot();
     });
 
     it('displays a secondary button with requested text', () => {
-        const component = renderer.create(
-            <Button theme={Bootstrap} type="secondary">
-                ⊙_ʘ
-            </Button>
-        );
+        const component = renderer.create(<Button type="secondary">⊙_ʘ</Button>);
         expect(component.toJSON()).toMatchSnapshot();
     });
 });
--- src/styleguidist/ThemeWrapper.jsx
+++ src/styleguidist/ThemeWrapper.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import theme from '../themes';
 
-export default props => <ThemeProvider {...props} theme={theme} />;
+export default props => <ThemeProvider {...props} theme={{}} />;
