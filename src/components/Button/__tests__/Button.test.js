1ac1ca8f899a056852aa1d0d9ed967760383323f
Brian Stone <brians@zillowgroup.com>
2019-06-22 15:10:55 -0700

    test: add additional `Button` tests

--- src/components/Button/__tests__/Button.test.js
+++ src/components/Button/__tests__/Button.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
-import { ThemeProvider, css } from 'styled-components';
+import { ThemeProvider, css, isStyledComponent } from 'styled-components';
 import { Button, withNamespace } from '../../../index';
 
 describe('<Button>', () => {
@@ -26,6 +26,15 @@ describe('<Button>', () => {
         );
         const tree = testRenderer.toJSON();
         expect(tree).toHaveStyleRule('color', 'red');
+        expect(tree).toMatchSnapshot();
+    });
+    it('is a styled-component', () => {
+        expect(isStyledComponent(Button)).toBe(true);
+    });
+    it('supports the as prop', () => {
+        const testRenderer = TestRenderer.create(<Button as="a">Test button</Button>);
+        const tree = testRenderer.toJSON();
+        expect(tree.type).toBe('a');
         expect(tree).toMatchSnapshot();
     });
 });
