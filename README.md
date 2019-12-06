379d7ca3e5da15656234670fee26a9c945612129
Brian Stone <brians@zillowgroup.com>
2019-12-06 10:13:32 -0800

    build(TODO): convert to a monorepo using lerna and pectin
    TODO: This updates the package to use the @zillow scope.
    Before we are done, we need to remove the scope and delete the .npmrc file.

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-[![npm version](https://badge.fury.io/js/drywall.svg)](https://badge.fury.io/js/drywall)
-[![Conventional Commits](https://img.shields.io/badge/Conventional%20Commits-1.0.0-yellow.svg)](https://conventionalcommits.org)
-
-Drywall is a style agnostic component library built with [React](https://reactjs.org/).
-The library consists of high quality, functional components that are accessible and thoroughly tested.
+MONOREPO
--- styleguide.config.js
+++ styleguide.config.js
@@ -3,16 +3,34 @@ const { createStyleguideConfig } = require('create-react-styleguide');
 
 module.exports = createStyleguideConfig({
     usageMode: 'expand',
+    webpackConfig: {
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: [/node_modules/],
+                    use: {
+                        loader: 'babel-loader',
+                    },
+                },
+            ],
+        },
+    },
     sections: [{
         name: 'Releases ↗',
         href: 'https://github.com/zillow/drywall/blob/master/CHANGELOG.md',
         external: true
     }, {
         name: 'Introduction',
-        content: 'README.md'
+        content: 'packages/drywall/README.md'
+    }, {
+        name: "Components",
+        components: "packages/drywall/src/**/[A-Z]*.jsx"
     }],
     getComponentPathLine: componentPath => {
         const name = path.basename(componentPath, '.jsx');
         return `import { ${name} } from 'drywall';`;
     }
+}, {
+    componentsSection: false,
 });
