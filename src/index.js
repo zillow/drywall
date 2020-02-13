7834b331e9c7d877ee8687bcc7eda5489489ff19
Brian Stone <brians@zillowgroup.com>
2020-02-12 17:11:44 -0800

    fix: forward refs when using the `withToast` HOC and export `ToastContext`

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ export { default as Button } from './components/Button/Button';
 export { default as CloseButton } from './components/CloseButton/CloseButton';
 export { default as MediaObject } from './components/MediaObject/MediaObject';
 export { default as Toast } from './components/Toast/Toast';
-export { default as ToastProvider, withToast } from './components/Toast/ToastProvider';
+export {
+    default as ToastProvider,
+    ToastContext,
+    withToast,
+} from './components/Toast/ToastProvider';
 export { default as withNamespace } from './theme/withNamespace';
 export { default as getTheme } from './theme/getTheme';
