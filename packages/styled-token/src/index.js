e6c6662f6ee27bcc24507ac7349aae8b9a740fd1
Brian Stone <brians@zillowgroup.com>
2020-03-30 10:02:29 -0700

    feat(styled-token): allow access to all values when using array path syntax, and add new object path syntax

--- packages/styled-token/src/index.js
+++ packages/styled-token/src/index.js
@@ -1,10 +1,53 @@
 import { stringToPath } from './stringToPath';
 
 /**
- * See README.md for full documentation.
+ * Helper function for getting the value for a path or a fallback array.
  *
  * @method
  * @param path {string|array}
+ * @param theme {object} The theme namespace.
+ * @return {object} The derived value and an array of all values.
+ */
+const getValues = (path, theme) => {
+    let paths = path;
+    if (typeof path === 'string') {
+        paths = [path];
+    }
+
+    // Translate the array of paths to an array of values
+    let value;
+    const values = paths.map(p => {
+        let currentValue = theme;
+
+        // Traverse the path
+        const properties = stringToPath(p);
+        for (let j = 0; j < properties.length && typeof currentValue !== 'undefined'; j += 1) {
+            const property = properties[j];
+            // Skip empty string properties
+            if (property) {
+                currentValue = currentValue[property];
+            }
+        }
+
+        // Set outer value to the first defined value
+        if (typeof value === 'undefined') {
+            value = currentValue;
+        }
+
+        return currentValue;
+    });
+
+    return {
+        value,
+        values,
+    };
+};
+
+/**
+ * See README.md for full documentation.
+ *
+ * @method
+ * @param path {string|array|object}
  * @param [options] {object}
  * @param [callback] {function}
  * @return {function}
@@ -17,56 +60,53 @@ export default (path, options = {}, callback) => props => {
         options = {};
     }
 
-    let val = props.theme;
+    let { theme } = props;
 
     // Check for namespace
-    let namespace = props.theme && props.theme.NAMESPACE;
+    let namespace = theme && theme.NAMESPACE;
     if (typeof options.namespace === 'string') {
         // eslint-disable-next-line prefer-destructuring
         namespace = options.namespace;
     }
 
     // Move to namespace
-    if (val && namespace) {
-        val = val[namespace];
+    if (theme && namespace) {
+        theme = theme[namespace];
     }
 
-    let paths = path;
-    if (typeof paths !== 'object') {
-        paths = [path];
-    }
-
-    // Iterate through paths
-    const originalVal = val;
-    for (let i = 0; i < paths.length; i += 1) {
-        const p = paths[i];
-
-        // Reset val if we need to try another path
-        val = originalVal;
-
-        // Traverse the path
-        const properties = stringToPath(p);
-        for (let j = 0; j < properties.length && typeof val !== 'undefined'; j += 1) {
-            const property = properties[j];
-            // Skip empty string properties
-            if (property) {
-                val = val[property];
-            }
+    let value;
+    let values;
+    if (typeof path === 'string' || Array.isArray(path)) {
+        const results = getValues(path, theme);
+        // eslint-disable-next-line prefer-destructuring
+        value = results.value;
+        // eslint-disable-next-line prefer-destructuring
+        values = results.values;
+    } else if (typeof path === 'object') {
+        if (!callback) {
+            throw new Error('`callback` is required when `path` is an `object`');
         }
 
-        // Break out of paths array once we find the first defined value
-        if (typeof val !== 'undefined') {
-            break;
-        }
+        value = {};
+        Object.keys(path).forEach(key => {
+            const results = getValues(path[key], theme);
+            value[key] = results.value;
+        });
+    } else {
+        throw new Error('`path` should be one of `string`, `array`, `object`');
     }
 
     // defaultValue fallback
-    if (typeof val === 'undefined') {
-        val = options.defaultValue;
+    if (typeof value === 'undefined') {
+        value = options.defaultValue;
     }
 
     if (callback) {
-        return callback(val);
+        // Arrays gets both the derived value and the list of values as arguments
+        if (Array.isArray(path)) {
+            return callback(value, values);
+        }
+        return callback(value);
     }
-    return val;
+    return value;
 };
